0a56707df3d50899995dfa671281271970e649b2
Lewis Warren <lewis@lewiss-imac.home>
2019-08-08 11:40:14 +0100

    Added Gulp task runner
    For SVG minifying and sprite sheet

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+
+
+var gulp = require('gulp');
+var svgstore = require('gulp-svgstore');
+var svgmin = require('gulp-svgmin');
+var path = require('path');
+var rename = require("gulp-rename");
+
+gulp.task('svgstore', function () {
+    return gulp
+        .src('img/**/*.svg' )
+        .pipe( svgmin( {
+    			plugins: [
+    				{ removeViewBox: false },
+    				{ removeUselessStrokeAndFill: true },
+    				{ removeEmptyAttrs: false },
+    				{ removeUnknownsAndDefaults: false },
+    				{ cleanupIDs: false },
+    				{ removeUselessStrokeAndFill: true }
+    			]
+    		} ) )
+        .pipe(svgstore())
+        .pipe( rename( 'icons.svg' ) )
+        .pipe(gulp.dest('img/'))
+});
